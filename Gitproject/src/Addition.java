41654f8f29824d7d3c4441b12d7581ac4bd1968f
Abhishekjlk <124226723+Abhishekjlk@users.noreply.github.com>
2023-02-02 09:17:33 +0530

    Add files via upload

---
+++ Gitproject/src/Addition.java
@@ -0,0 +1,17 @@
+
+public class Addition {
+	
+	    
+	    public static void main(String[] args) {
+	        
+	        int x = 89; 
+	        
+	        int y = 43;
+	        
+	        int sum = x + y;
+	        
+	        System.out.println("Sum is " + sum);
+	    
+	}
+
+}
